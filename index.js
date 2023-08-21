7390f4d4e121860582c66da919c05cb01704d22e
Charlie <charliemoomoo@yandex.com>
2023-08-21 09:00:40 +0800

    new link format

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ const express = require("express")
 const multer = require("multer")
 const upload = multer({ dest: "uploads/" })
 const fs = require("fs")
-const uuid = require("uuid").v4
 const ncr = require("ncr-decode")
 
 const app = express()
 const baselink = process.env.BaseLink
+
+function generateID(length) {
+  let result = ""
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-"
+  const charactersLength = characters.length
+  let counter = 0
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    counter += 1
+  }
+  if (fs.existsSync(`files/${result}`)) {
+    result = generateID(length)
+  }
+  return result
+}
 
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname })
@@ -19,9 +34,7 @@ app.get("/favicon.ico", (req, res) => {
   res.sendFile("favicon.ico", { root: __dirname })
 })
 app.post("/upload", upload.array("files"), (req, res) => {
-  const folderid = String(
-    parseInt(uuid().replaceAll("-", ""), 16) / 10000000000000000000000
-  )
+  var folderid = generateID(5)
   fs.mkdirSync("files/" + folderid, { recursive: true })
   req.files.forEach((file) => {
     fs.renameSync(
@@ -31,21 +44,28 @@ app.post("/upload", upload.array("files"), (req, res) => {
   })
   const filelink =
     req.files.length != 1
-      ? baselink + "/download/" + folderid
-      : baselink + "/download/" + folderid + "/" + req.files[0].originalname
-  if (req.query.sharex == "1") return res.send(filelink + "?sharex=1")
+      ? req.query.sharex != "1"
+        ? baselink + "/d/" + folderid
+        : baselink + "/s/" + folderid
+      : baselink + "/d/" + folderid + "/" + req.files[0].originalname
+  if (req.query.sharex == "1") return res.send(filelink)
   res.send(
     fs.readFileSync("link.html").toString().replaceAll("{link}", filelink)
   )
 })
-app.get("/download/:id/:name", (req, res) => {
+app.get("/d/:id/:name", (req, res) => {
   const filepath = __dirname + "/files/" + req.params.id + "/" + req.params.name
   if (!fs.existsSync(filepath))
     return res.status(404).sendFile("404.html", { root: __dirname })
-  if (req.query.sharex == "1") return res.sendFile(filepath)
   res.download(filepath)
 })
-app.get("/download/:id", (req, res) => {
+app.get("/s/:id/:name", (req, res) => {
+  const filepath = __dirname + "/files/" + req.params.id + "/" + req.params.name
+  if (!fs.existsSync(filepath))
+    return res.status(404).sendFile("404.html", { root: __dirname })
+  res.sendFile(filepath)
+})
+app.get("/d/:id", (req, res) => {
   if (!fs.existsSync("files/" + req.params.id))
     return res.status(404).sendFile("404.html", { root: __dirname })
   let linklist = []
@@ -62,6 +82,10 @@ app.get("/download/:id", (req, res) => {
   )
 })
 app.use((req, res) => {
+  if (req.path.startsWith("/download/")) {
+    // old link redirect
+    res.redirect(req.path.replace("/download/", "/d/"))
+  }
   res.status(404).sendFile("404.html", { root: __dirname })
 })
 app.listen(process.env.Port)
